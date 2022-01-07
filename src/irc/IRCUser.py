07383c56f2c5b9e742354c5ccf9dd16d39aa7d92
Jordan Schneidler <jordan.schneidler@gmail.com>
2022-01-07 15:52:21 -0700

    User modes

--- src/irc/IRCUser.py
+++ src/irc/IRCUser.py
@@ -1,15 +1,55 @@
+from dataclasses import dataclass
 from typing import Optional
 
 MAX_NICK_LENGTH = 9
 
 
+@dataclass
+class Modes:
+    away: bool = False
+    invisible: bool = False
+    operator: bool = False
+
+
 class User:
     password: Optional[str] = None
-    mode: Optional[int] = None
     nick: str = "*"
     username: Optional[str] = None
     realname: Optional[str] = None
-    isOperator: bool = False
+    modes: Modes = Modes()
+
+    def setNick(self, nick: str) -> None:
+        if len(nick) > MAX_NICK_LENGTH:
+            raise ValueError("Nick is too long")
+        self.nick = nick
+
+    def getModes(self) -> str:
+        modes = ""
+        if self.isAway():
+            modes += "a"
+        if self.isInvisible():
+            modes += "i"
+        if self.isOperator():
+            modes += "o"
+        return modes
+
+    def isAway(self) -> bool:
+        return self.modes.away
+
+    def setAway(self, away: bool) -> None:
+        self.modes.away = away
+
+    def isInvisible(self) -> bool:
+        return self.modes.invisible
+
+    def setInvisible(self, invisible: bool) -> None:
+        self.modes.invisible = invisible
+
+    def isOperator(self) -> bool:
+        return self.modes.operator
+
+    def setOperator(self, operator: bool) -> None:
+        self.modes.operator = operator
 
 
 Users = dict[str, User]
